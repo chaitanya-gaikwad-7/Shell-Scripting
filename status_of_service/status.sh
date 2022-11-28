4aaa59f0fa4cf3ba0ff4d6d1a8083729f17a59eb
Chaitanya Gaikwad <79074929+chaitanya-gaikwad-7@users.noreply.github.com>
2022-11-28 16:51:54 +0100

    Multiple Service status script

---
+++ status_of_service/status.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#PURPOSE: Check the status of all jenkins/services present on the server
+#AUTHOR: Chaitanya Gaikwad
+
+
+i=1
+input="services.txt"
+rm status.txt
+
+# Read each line from input file
+while IFS= read -r line  
+do
+  #serial number and service name 
+  echo $i $line >> status.txt 
+  # service status | Active status to status.txt  
+  systemctl status $line | grep Active >> status.txt
+  #increment serial number 
+  i=$(($i+1))
+done < "$input"
+
