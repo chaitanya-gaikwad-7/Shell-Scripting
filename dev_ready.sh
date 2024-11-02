da302dcafa9005abbffad660466c67798444637f
Chaitanya <chaitanya@example.com>
2024-11-02 07:23:26 +0530

    Shell scrip to make ubuntu laptop dev ready

--- dev_ready.sh
+++ dev_ready.sh
@@ -2,70 +2,78 @@
 
 #PURPOSE: Install the necessary software and make laptop development ready.
 #AUTHOR: Chaitanya Gaikwad
-#MODIFIED ON: 04.11.2021
-#NOTE: java7, sqldeveloper and Globalprotect VPN is downloaded before running this script.
+#MODIFIED ON: 02.11.2024
 
 #Update and Upgrade
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt update -y 
+sudo apt upgrade -y
+
+#JAVA
+sudo apt install openjdk-11-jdk
+
+#registering Python versions
+cd /usr/bin
+rm python python3
+sudo ln -s python2.7 python
+sudo ln -s python3.6 python3
+
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
+
+sudo update-alternatives --set python /usr/bin/python3
+
+#Python-pip
+sudo apt install python-pip
+sudo apt install python3-pip
 
 #GIT
 sudo apt install git
-
-#GRADLE
-sudo apt install gradle
 
 #Virtualbox
 sudo apt install virtualbox
 sudo apt install virtualbox-qt
 
-#Vagrant
-sudo apt install vagrant
+#Visual Studio Code
+cd Downloads
+wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+code_LTS=$(ls | grep -i deb | grep -i code)
+sudo apt install ./$(code_LTS)
 
-#Python
-sudo apt-get install python-pip
-sudo apt-get install python3-pip
+# Docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
 
-#JAVA 8
-sudo apt install openjdk-8-jdk
+# Kubernetes ( Micro K8s )
+sudo snap install microk8s --classic
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+su - $USER
 
-#JAVA7
-cd $HOME/Downloads
-if [ -e jdk-7u80-linux-x64.tar.gz ]
-then
-	sudo tar xzf jdk-7u80-linux-x64.tar.gz
-	sudo cp -r jdk1.7.0_80 /usr/lib/jvm/
-fi
-cd $HOME
+# Terraform
+cd Downloads
+git clone https://github.com/hashicorp/terraform.git
+cd terraform
+go install
+echo $PATH
+mv ~/Downloads/terraform /usr/local/bin/
+touch ~/.bashrc
+terraform -install-autocomplete
 
-#Eclipse
-sudo apt install snap
-sudo snap install --classic eclipse
-
-#SQL Developer
-cd $HOME/Downloads
-if [ -e sqldeveloper_20.4.1-408.0006_all.deb ]
-then
-	sudo dpkg -i sqldeveloper_20.4.1-408.0006_all.deb
-fi
-cd $HOME
-
-#Globalprotect VPN
-cd $HOME/Downloads
-if [ -e GlobalProtect_UI_deb-5.3.0.0-32.deb ]
-then
-	sudo dpkg -i GlobalProtect_UI_deb-5.3.0.0-32.deb
-fi
-cd $HOME
-
-#Nameserver
-nameserver=ip_of_server
-sudo echo -e "nameserver $nameserver" >> /etc/resolv.conf
-
-#registering java versions
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.7.0_80/bin/java 1
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/bin/java 2
-
-#registering Python versions
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python2.7 1
-sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+# ansible
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
